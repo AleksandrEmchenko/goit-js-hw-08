d8fb2fcbee4352fb22113b3893282bf5bc0d4a9d
AleksandrEmchenko <44650107+AleksandrEmchenko@users.noreply.github.com>
2023-03-11 19:11:25 +0000

    Deploying to gh-pages from @ AleksandrEmchenko/goit-js-hw-08@ca72c80b65771d789b91de878afe6b661c545149 🚀

--- 02-video.9559c63f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}));o.on("play",(function(e){o.addEventListener("onPlay")})),localStorage.setItem("videoplayer-current-time",currentTime)}();
-//# sourceMappingURL=02-video.9559c63f.js.map
