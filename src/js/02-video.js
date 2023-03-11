f0011d90fb286009543c5ed44bad1a0585738d31
Aleksandr Yemchenko <aleksandremchenko@gmail.com>
2023-03-11 16:49:55 +0200

    complette tasks 1, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+// Ініціалізація
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+// ---------
+
+
+
+const onPlay = function(data) {
+    player.addEventListener('onPlay')
+};
+
+player.on('play', onPlay);
+
+
+
+
+localStorage.setItem("videoplayer-current-time", currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "feedback-form-state";
+
+const formEl= document.querySelector('.feedback-form');
+const emailEl= document.querySelector('[name="email"]');
+const messageEl= document.querySelector('[name="message"]');
+
+formEl.addEventListener("submit", handleFormControl);
+formEl.addEventListener('input', throttle(handleformFildInput, 500));
+
+fillingFilds();
+
+function handleformFildInput(){
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({ email: emailEl.value, message: messageEl.value }));
+}
+
+function handleFormControl(event) {
+  event.preventDefault();
+
+  console.log({ email: emailEl.value, message: messageEl.value });
+
+  event.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function fillingFilds (){
+    const formData = localStorage.getItem(STORAGE_KEY);
+    
+    if(formData){
+       const formObject = JSON.parse(formData);
+       emailEl.value = formObject.email;
+       messageEl.value = formObject.message;
+}
+}
